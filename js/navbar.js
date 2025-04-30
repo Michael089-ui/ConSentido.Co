3727d0c3b56c5ab2b65e153a21794cd72ccd18a9
brigittechavez <brigitte.chavez@correounivalle.edu.co>
2025-04-29 19:07:16 -0500

    Entrega 6 (navbar)

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const logo = document.querySelector(".logo-img");
-  
-    if (logo) {
-      setInterval(() => {
-        logo.classList.add("logo-grow");
+document.addEventListener("DOMContentLoaded", function () {
+  const logo = document.querySelector(".logo-img");
 
-        setTimeout(() => {
-          logo.classList.remove("logo-grow");
-        }, 1000);
-  
-      }, 5000);
-    }
-  });
+  if (logo) {
+    setInterval(() => {
+      logo.classList.add("logo-grow");
+
+      setTimeout(() => {
+        logo.classList.remove("logo-grow");
+      }, 1000);
+
+    }, 5000);
+  }
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+  fetch("/components/navbar.html") // o "../components/navbar.html" si está en otra carpeta
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("nav-placeholder").innerHTML = data;
+    });
+});
+
+// $(document).ready(function () {
+//   $('#nav-placeholder').load('/components/nav.html', function (response, status, xhr) {
+//     if (status == 'error') {
+//       console.log('Error loading nav:', xhr.status, xhr.statusText);
+//     }
+//   });
+// });
